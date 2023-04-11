6f4aaa46b7f86b62d3f0d916ac3d4ea1b4f777e6
Bruna557 <schroder.bruna@hotmail.com>
2023-04-11 15:14:01 -0300

    Style post card

--- frontend/src/components/PostCard/PostCard.tsx
+++ frontend/src/components/PostCard/PostCard.tsx
@@ -1,5 +1,7 @@
-import Button from 'react-bootstrap/Button';
+import { Button, Row, Col } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
+import { faThumbsUp } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 import { Post } from "../../types"
 import "./PostCard.scss"
@@ -8,10 +10,21 @@ const PostCard = (post: Post) => {
   return (
     <>
       <Card>
-        <Card.Img variant="top" src={post.picture} />
+        <Row>
+          <Col>
+            <Card.Img variant="top" src={post.picture} />
+          </Col>
+          <Col className="posted-on">{post.posted_on.substring(5, 25)}</Col>
+        </Row>
         <Card.Body>
           <Card.Text>{post.body}</Card.Text>
-          <Button variant="primary">Like</Button>
+          <Row>
+            <Col lg="12" className="likes">
+              {post.likes}
+              <Button className="like-button"><FontAwesomeIcon icon={faThumbsUp} /></Button>
+            </Col>
+
+          </Row>
         </Card.Body>
       </Card>
     </>
